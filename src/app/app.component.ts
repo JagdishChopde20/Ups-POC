490cd4b75568b8a7465d92eeb03b3b0f87bff48c
Jagdish Chopde <jagdishchopde@outlook.com>
2019-07-31 03:43:58 +0530

    POC Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ups-poc';
+  title = 'UPS-POC';
+
+  private fieldArray: Array<any> = [];
+  private newAttribute: any = {};
+
+  addFieldValue() {
+    this.fieldArray.push(this.newAttribute)
+    this.newAttribute = {};
+  }
+
+  deleteFieldValue(index) {
+    this.fieldArray.splice(index, 1);
+  }
+
+  log(code) {
+    console.log(code);
+  }
+  
+  onSubmit(f){
+    this.addFieldValue();
+    f.form.reset();
+  }
 }
